46441c54b59db5e00dc6e682d55b434003d56e8a
tommycbird <tommycbird@gmail.com>
2025-10-22 11:16:35 -0600

    new icon component - unused

---
+++ components/beta/metal-icon.tsx
@@ -0,0 +1,6 @@
+/** @paper-design/shaders-react@0.0.60 */
+import { LiquidMetal as LiquidMetal1 } from '@paper-design/shaders-react';
+
+export default function MetalIcon() {
+  return <LiquidMetal1 image="/img/branding/icon-dark.png" speed={0.5} colorBack="#00000000" colorTint="#FFFFFF" softness={0.1} repetition={2} shiftRed={0.3} shiftBlue={0.3} distortion={0.07} contour={0.4} scale={0.6} rotation={0} shape="diamond" angle={70} style={{ backgroundColor: '#00000000', borderRadius: '12px', height: '516px', width: '516px' }} />;
+}
